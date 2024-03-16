985dfc7c26d70941d8559630ef9d54721fa8c1b4
SmgTR <smagokpnd@gmail.com>
2024-03-16 15:50:51 +0100

    update next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '**',
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'images.unsplash.com',
+    //     port: '',
+    //     pathname: '**',
+    //   },
+    // ],
   },
   experimental: {
     serverActions: {
