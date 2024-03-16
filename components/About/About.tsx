4e17949f8ee3fd3f038a34987abb37a709fd49a4
SmgTR <smagokpnd@gmail.com>
2024-03-16 20:00:00 +0100

    added about section

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,12 +1,25 @@
+import SectionWrapper from '../Wrappers/SectionWrappers';
+import Image from 'next/image';
+import catAbout from '@/public/cat-about.png';
 const About = () => {
   return (
-    <section>
-      <h2>O nas</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
-        obcaecati!
-      </p>
-    </section>
+    <SectionWrapper>
+      <div className='flex justify-between items-center'>
+        <Image
+          src={catAbout}
+          height={180}
+          alt='Kobieta z kotem uprawiajaca yoge'
+        />
+        <div className='max-w-prose'>
+          <h2 className='text-5xl font-madimi'>O nas </h2>
+          <p className='mt-8'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            temporibus praesentium dolore laborum optio asperiores sed alias
+            reiciendis vitae debitis.
+          </p>
+        </div>
+      </div>
+    </SectionWrapper>
   );
 };
 
