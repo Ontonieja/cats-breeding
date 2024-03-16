511504647fd3ca53a003b647d18f1c09d2453194
SmgTR <smagokpnd@gmail.com>
2024-03-16 19:59:44 +0100

    added icons

--- components/Icons/Icons.tsx
+++ components/Icons/Icons.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { IconContext } from 'react-icons';
+import { IoPaw } from 'react-icons/io5';
 import { FaArrowCircleRight } from 'react-icons/fa';
+import { FaChevronRight } from 'react-icons/fa6';
 
 export const RightArrow = () => {
   return (
@@ -12,6 +14,32 @@ export const RightArrow = () => {
       }}
     >
       <FaArrowCircleRight />
+    </IconContext.Provider>
+  );
+};
+
+export const PawLogo = () => {
+  return (
+    <IconContext.Provider
+      value={{
+        color: 'coral-red',
+        className: 'global-class-name text-coral-red text-4xl mr-2',
+      }}
+    >
+      <IoPaw />
+    </IconContext.Provider>
+  );
+};
+
+export const ChevronCircle = () => {
+  return (
+    <IconContext.Provider
+      value={{
+        color: 'coral-red',
+        className: 'global-class-name bg-white rounded-full',
+      }}
+    >
+      <FaChevronRight />
     </IconContext.Provider>
   );
 };
