288967f9e07ce8cec71dc4e9deb1e12009b7ff62
„SmgTR” <smagokpnd@gmail.com>
2024-03-17 10:07:09 +0100

    fix minor issues with imports

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,5 +1,5 @@
+import Nav from '@/components/Nav/Nav';
 import { ReactNode } from 'react';
-import Nav from '@/components/Nav';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
---
+++ components/Nav.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import React from 'react';
+import { IoPaw } from 'react-icons/io5';
+
+const Nav = () => {
+  return (
+    <nav className='flex justify-between item-center p-2'>
+      <div className='flex items-center font-madimi'>
+        <IoPaw className='size-8 mr-2' />
+        Samo szczęście
+      </div>
+      <ul className='flex gap-4'>
+        <li>
+          <Link href='/'>Strona główna</Link>
+        </li>
+        <li>
+          <Link href='/kocury'>Kocury</Link>
+        </li>
+        <li>
+          <Link href='/kotki'>Kotki</Link>
+        </li>
+        <li>
+          <Link href='/kocieta'>Kocięta</Link>
+        </li>
+        <li>
+          <Link href='/kontakt'>Kontakt</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
