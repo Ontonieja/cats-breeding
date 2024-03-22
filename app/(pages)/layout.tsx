2c0f8ae9342a472dd40ab849c2f38bea47d7ec7f
Maksymilian Rusnak <ontonieja@gmail.com>
2024-03-22 16:58:32 +0100

    added footer

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,11 +1,13 @@
 import Nav from "@/components/Nav/Nav";
 import { ReactNode } from "react";
+import Footer from "@/components/Footer/Footer";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Nav />
       <main>{children}</main>
+      <Footer></Footer>
     </>
   );
 }
---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="bg-[#444] min-h-52  ">
+      <div className="max-container py-12 max-sm:px-8 ">
+        <h2 className="text-3xl text-white font-madimi max-sm:text-2xl">
+          {" "}
+          Samo Sczęście*PL{" "}
+        </h2>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
