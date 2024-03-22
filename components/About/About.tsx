6dd226d97a16a585bea2d1cd5293687d19a53245
Maksymilian Rusnak <ontonieja@gmail.com>
2024-03-22 16:59:39 +0100

    fixed about section

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -5,15 +5,15 @@ import catAbout from "@/public/cat-about.png";
 const About = () => {
   return (
     <SectionWrapper>
-      <div className="flex justify-between items-center max-container ">
+      <div className="flex justify-between items-center max-container max-sm:flex-col max-sm:items-start">
         <Image
           src={catAbout}
           height={180}
           alt="Kobieta z kotem uprawiajaca yoge"
-          className="max-sm:hidden"
+          className="max-sm:w-[150px] max-sm:mb-2"
         />
-        <div className="max-w-prose max-sm:text-center ">
-          <h2 className="text-4xl font-madimi">O nas </h2>
+        <div className="max-w-prose ">
+          <h2 className="text-4xl font-madimi max-sm:jusify">O nas </h2>
           <p className="mt-4 text-lg">
             Jesteśmy siostrami z pasją do kotów brytyjskich, prowadzącymi
             hodowlę Samo Szczęście. Nasza misja to zapewnienie najlepszych
