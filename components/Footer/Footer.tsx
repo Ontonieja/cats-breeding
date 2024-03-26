623a1c84537ec9831794730186e644e04d252b1b
Maksymilian Rusnak <ontonieja@gmail.com>
2024-03-26 13:15:21 +0100

    adjusted footer for mobile devices

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,30 @@
+import { PawLogo } from "../Icons/Icons";
+import catsHead from "@/public/cat-head.png";
+import Image from "next/image";
+import SectionWrapper from "../Wrappers/SectionWrappers";
 const Footer = () => {
   return (
-    <footer className="bg-[#444] min-h-52  ">
-      <div className="max-container py-12 max-sm:px-8 ">
-        <h2 className="text-3xl text-white font-madimi max-sm:text-2xl">
-          {" "}
-          Samo Sczęście*PL{" "}
-        </h2>
+    <footer className="padding-t max-w-full">
+      <div className="bg-[#434343] min-h-44 rounded-t-3xl flex items-center text-white justify-between sm:px-12 px-4 relative  max-sm:gap-16">
+        <div className="flex text-white font-madimi max-sm:flex-1 items-center ">
+          <PawLogo />
+
+          <span className="text-xl  leading-none">
+            Samo <br></br>Szczęście
+          </span>
+        </div>
+        <div className="text-xl max-sm:text-sm">
+          email: <b>samoszczescie@wp.pl</b> tel: <b>+48 535 534 030</b>
+        </div>
+        <div className="text-xl max-sm:text-sm max-sm:hidden">
+          Warszawa <br />
+          30-353 Wielickiego 11
+        </div>
+        <Image
+          src={catsHead}
+          alt="głowa kota"
+          className="absolute left-1/2 right-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-10  "
+        />
       </div>
     </footer>
   );
