146bdd8049b620689f3c37316b28f3651a80d6fd
Maksymilian Rusnak <ontonieja@gmail.com>
2024-03-29 11:46:00 +0100

    changed prisma schema

---
+++ db/prisma/migrations/20240329074807_new/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Cat" ADD COLUMN     "Mother" TEXT,
+ADD COLUMN     "birthday" TEXT,
+ADD COLUMN     "blood" TEXT,
+ADD COLUMN     "color" TEXT,
+ADD COLUMN     "father" TEXT;
