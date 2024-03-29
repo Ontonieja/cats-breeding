71c209083a0a7eb00f795a6beabb75886bc5ef4a
Maksymilian Rusnak <ontonieja@gmail.com>
2024-03-29 11:44:32 +0100

    added mobile navbar

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -4,12 +4,23 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { PawLogo } from "../Icons/Icons";
 import { HamburgerIcon } from "../Icons/Icons";
+import {
+  AiOutlineClose,
+  AiOutlineFacebook,
+  AiOutlineInstagram,
+} from "react-icons/ai";
+import { useState } from "react";
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleNav = () => {
+    setMenuOpen(!menuOpen);
+    console.log(menuOpen);
+  };
   const pathname = usePathname();
   return (
     <header className="padding-x py-8">
-      <nav className="sticky flex justify-between items-center max-container">
+      <nav className="flex justify-between items-center max-container">
         <Link href="/" className="flex items-center font-madimi">
           <PawLogo />
 
@@ -17,10 +28,10 @@ const Nav = () => {
             Samo <br></br>Szczęście
           </span>
         </Link>
-        <div className="lg:hidden block">
+        <div className="md:hidden block" onClick={handleNav}>
           <HamburgerIcon />
         </div>
-        <ul className="gap-16 text-xl font-medium hidden lg:flex">
+        <ul className="gap-16 text-xl font-medium hidden md:flex max-lg:gap-6">
           <li>
             <Link
               href="/"
@@ -62,6 +73,57 @@ const Nav = () => {
             </Link>
           </li>
         </ul>
+        <div
+          className={
+            menuOpen
+              ? "fixed right-0 top-0 w-[65%] sm:hidden h-screen bg-[#F5F5F5] p-10 ease-in duration-500 shadow-card-strong z-20"
+              : "fixed right-[-100%] top-0 p-10 ease-out duration-500 h-screen "
+          }
+        >
+          <div className="flex w-full items-center justify-end ">
+            <div onClick={handleNav} className="cursor-pointer ">
+              <AiOutlineClose size={25}></AiOutlineClose>
+            </div>
+          </div>
+          <div className="flex-col py-4 ">
+            <ul>
+              <Link href="/">
+                <li onClick={handleNav} className="py-4 cursor-pointer">
+                  Strona główna
+                </li>
+              </Link>
+              <Link href="/kocury">
+                <li onClick={handleNav} className="py-4 cursor-pointer">
+                  Kocury
+                </li>
+              </Link>
+              <Link href="/kocieta">
+                <li onClick={handleNav} className="py-4 cursor-pointer">
+                  Kocięta
+                </li>
+              </Link>
+              <Link href="/kontakt">
+                <li onClick={handleNav} className="py-4 cursor-pointer">
+                  Kontakt
+                </li>
+              </Link>
+            </ul>
+          </div>
+          <div className="flex justify-evenly pt-10 items-center">
+            <Link
+              href="https://www.instagram.com/samo_szczescie_cattery/"
+              target="_blank"
+            >
+              <AiOutlineInstagram size={30}></AiOutlineInstagram>
+            </Link>
+            <Link
+              href="https://www.facebook.com/SamoSzczesciePL"
+              target="_blank"
+            >
+              <AiOutlineFacebook size={30}></AiOutlineFacebook>
+            </Link>
+          </div>
+        </div>
       </nav>
     </header>
   );
