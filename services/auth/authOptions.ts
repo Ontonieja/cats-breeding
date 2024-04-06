3b2ddd4468e54a8bd87f87b2d8e60f5258d2a060
SmgTR <smagokpnd@gmail.com>
2024-04-06 13:26:15 +0200

    update options

--- services/auth/authOptions.ts
+++ services/auth/authOptions.ts
@@ -2,27 +2,27 @@ import {
   getServerSession,
   type NextAuthOptions,
   type Session,
-} from "next-auth";
+} from 'next-auth';
 
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from 'next-auth/providers/google';
 
 const { NEXTAUTH_SECRET, WHITELISTED_EMAILS } = process.env;
 
-const emailWhitelist = ["smagokpnd@gmail.com", "ontonieja@gmail.com"];
+const emailWhitelist = ['smagokpnd@gmail.com', 'ontonieja@gmail.com'];
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID || "",
-      clientSecret: process.env.GOOGLE_SECRET || "",
+      clientId: process.env.GOOGLE_ID || '',
+      clientSecret: process.env.GOOGLE_SECRET || '',
     }),
   ],
   secret: NEXTAUTH_SECRET,
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        if (!emailWhitelist.includes(user.email || "")) {
-          throw new Error("Invalid email");
+        if (!emailWhitelist.includes(user.email || '')) {
+          throw new Error('Invalid email');
         }
       }
       return token;
@@ -38,6 +38,6 @@ export const getServerAuthSession = () => getServerSession(authOptions);
 
 export const checkSession = (session: Session | null) => {
   if (!session) {
-    throw Error("Session does not exists");
+    throw Error('Session does not exists');
   }
 };
