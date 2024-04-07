0f953fff7effe3da5bdfb42eb04032b497335f3c
SmgTR <smagokpnd@gmail.com>
2024-04-07 16:11:42 +0200

    fix gallery

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -5,12 +5,12 @@ const Gallery = async () => {
   const catsImages = await getGalleryImages();
 
   return (
-    <div className="w-screen sm:w-auto sm:grid sm:grid-cols-1 md:grid-cols-4 auto-rows-[200px] gap-4 my-10 overflow-x-scroll sm:overflow-visible flex flex-nowrap sm:flex-wrap relative left-1/2 sm:left-auto transform translate-x-[-50%] sm:translate-x-0 ml-8 sm:ml-0">
+    <div className="sm:grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 auto-rows-[200px] gap-4 my-10 overflow-x-scroll sm:overflow-visible flex flex-nowrap sm:flex-wrap">
       {catsImages.map(({ url, id }, index) => {
         return (
           <div
             key={id}
-            className={`h-full shadow-card w-full rounded-xl overflow-hidden  min-w-0 ${
+            className={`h-full shadow-card w-full rounded-xl overflow-hidden max-sm:min-w-[350px] ${
               index === 2
                 ? 'sm:row-start-1 sm:row-end-3 sm:col-start-2 sm:col-end-4'
                 : ''
@@ -29,5 +29,4 @@ const Gallery = async () => {
     </div>
   );
 };
-
 export default Gallery;
--- components/Sections/Section.tsx
+++ components/Sections/Section.tsx
@@ -17,7 +17,7 @@ const Section: FC<SectionProps> = ({
   imageFirst = false,
 }) => {
   return (
-    <div className="flex justify-between items-center max-container max-sm:flex-col max-sm:items-start">
+    <div className="flex justify-between items-center max-container max-sm:flex-col max-sm:items-start overflow-hidden">
       <div className="max-w-prose">
         <h2 className="text-4xl font-madimi">{title}</h2>
         <div className="mt-4 text-xl leading-8">{children}</div>
