79bab25718b983961292e8016a194c5db0c61ba7
Maksymilian Rusnak <ontonieja@gmail.com>
2024-04-15 18:47:24 +0200

    made nav more reusable

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -10,7 +10,7 @@ import {
   AiOutlineInstagram,
 } from 'react-icons/ai';
 import { useState } from 'react';
-
+import { NavDesktopElement, NavMobileElement } from './NavElements';
 const Nav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const handleNav = () => {
@@ -31,47 +31,11 @@ const Nav = () => {
         <div className="md:hidden block" onClick={handleNav}>
           <HamburgerIcon />
         </div>
-        <ul className="gap-16 text-xl font-medium hidden md:flex max-lg:gap-6">
-          <li>
-            <Link
-              href="/"
-              className={`${pathname === '/' ? 'text-coral-red' : ''} `}
-            >
-              Strona główna
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/kocury"
-              className={`${pathname === '/kocury' ? 'text-coral-red' : ''} `}
-            >
-              Kocury
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/kotki"
-              className={`${pathname === '/kotki' ? 'text-coral-red' : ''} `}
-            >
-              Kotki
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/kocieta"
-              className={`${pathname === '/kocieta' ? 'text-coral-red' : ''} `}
-            >
-              Kocięta
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/kontakt"
-              className={`${pathname === '/kontakt' ? 'text-coral-red' : ''} `}
-            >
-              Kontakt
-            </Link>
-          </li>
+        <ul className="gap-24 text-xl font-medium hidden md:flex max-lg:gap-6">
+          <NavDesktopElement title="Strona główna" href="/" />
+          <NavDesktopElement title="Kocury" href="/kocury" />
+          <NavDesktopElement title="Kocięta" href="/kocieta" />
+          <NavDesktopElement title="Kontakt" href="/kontakt" />
         </ul>
         <div
           className={
@@ -87,26 +51,26 @@ const Nav = () => {
           </div>
           <div className="flex-col py-4 ">
             <ul>
-              <Link href="/">
-                <li onClick={handleNav} className="py-4 cursor-pointer">
-                  Strona główna
-                </li>
-              </Link>
-              <Link href="/kocury">
-                <li onClick={handleNav} className="py-4 cursor-pointer">
-                  Kocury
-                </li>
-              </Link>
-              <Link href="/kocieta">
-                <li onClick={handleNav} className="py-4 cursor-pointer">
-                  Kocięta
-                </li>
-              </Link>
-              <Link href="/kontakt">
-                <li onClick={handleNav} className="py-4 cursor-pointer">
-                  Kontakt
-                </li>
-              </Link>
+              <NavMobileElement
+                title="Strona główna"
+                href={'/'}
+                onClick={handleNav}
+              />
+              <NavMobileElement
+                title="Kocury"
+                href={'/kocury'}
+                onClick={handleNav}
+              />
+              <NavMobileElement
+                title="Kocięta"
+                href={'/kocieta'}
+                onClick={handleNav}
+              />
+              <NavMobileElement
+                title="Kontakt"
+                href={'/kontakt'}
+                onClick={handleNav}
+              />
             </ul>
           </div>
           <div className="flex justify-evenly pt-10 items-center">
