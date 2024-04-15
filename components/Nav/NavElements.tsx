4b0123196630e641a2022ba232c890941148c0aa
Maksymilian Rusnak <ontonieja@gmail.com>
2024-04-15 18:47:39 +0200

    more reusable nav

---
+++ components/Nav/NavElements.tsx
@@ -0,0 +1,39 @@
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+interface NavDesktopElementProps {
+  title: string;
+  href: string;
+}
+
+interface NavMobileElementProps {
+  title: string;
+  href: string;
+  onClick: () => void;
+}
+export function NavDesktopElement({ title, href }: NavDesktopElementProps) {
+  const pathname = usePathname();
+  return (
+    <li>
+      <Link
+        href={href}
+        className={`${pathname === href ? 'text-coral-red' : ''} `}
+      >
+        {title}
+      </Link>
+    </li>
+  );
+}
+
+export function NavMobileElement({
+  title,
+  href,
+  onClick,
+}: NavMobileElementProps) {
+  const pathname = usePathname();
+  return (
+    <Link href={href} onClick={onClick}>
+      <li className="py-4 cursor-pointer">{title} </li>
+    </Link>
+  );
+}
