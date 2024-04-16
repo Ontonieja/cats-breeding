b0bde1f0da3e8ece395f50fc4ba728f4f9695f28
Maksymilian Rusnak <ontonieja@gmail.com>
2024-04-16 15:29:21 +0200

    fixed wrong database saving

--- app/admin/cats/edit/[id]/page.tsx
+++ app/admin/cats/edit/[id]/page.tsx
@@ -52,7 +52,7 @@ const EditCat: FC = () => {
           birthday: catInfo?.birthday || '',
           color: catInfo?.color || '',
           blood: catInfo?.blood || '',
-          father: catInfo?.birthday || '',
+          father: catInfo?.father || '',
           Mother: catInfo?.Mother || '',
         });
       }
@@ -64,7 +64,7 @@ const EditCat: FC = () => {
   const handleChange = (
     event: ChangeEvent<
       HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
-    >
+    >,
   ) => {
     setFormValues({
       ...formValues,
@@ -87,100 +87,100 @@ const EditCat: FC = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className='max-w-xl mx-auto mt-10 bg-white p-10 rounded shadow-md'
+      className="max-w-xl mx-auto mt-10 bg-white p-10 rounded shadow-md"
     >
-      <h2 className='mb-6 text-[24px]'>Dodaj kota</h2>
-      <div className='mb-5'>
-        <label className='block text-gray-700'>Imię:</label>
+      <h2 className="mb-6 text-[24px]">Dodaj kota</h2>
+      <div className="mb-5">
+        <label className="block text-gray-700">Imię:</label>
         <input
-          title='name'
-          type='text'
-          name='name'
+          title="name"
+          type="text"
+          name="name"
           value={formValues.name}
           onChange={handleChange}
-          className='mt-1 p-2 w-full border-2 border-gray-300 rounded'
+          className="mt-1 p-2 w-full border-2 border-gray-300 rounded"
         />
       </div>
-      <div className='mb-5'>
-        <label className='block text-gray-700'>Grupa:</label>
+      <div className="mb-5">
+        <label className="block text-gray-700">Grupa:</label>
         <SelectField
-          name='genderGroup'
+          name="genderGroup"
           value={formValues.genderGroup}
           options={['Kocury', 'Kotki', 'Kocięta']}
           onChange={handleChange}
-          className='mt-1 p-2 w-full border-2 border-gray-300 rounded'
-          title='genderGroup'
+          className="mt-1 p-2 w-full border-2 border-gray-300 rounded"
+          title="genderGroup"
         />
       </div>
-      <div className='mb-5'>
-        <label className='block text-gray-700'>Data urodzin:</label>
+      <div className="mb-5">
+        <label className="block text-gray-700">Data urodzin:</label>
         <input
-          title='birthday'
-          type='date'
-          name='birthday'
+          title="birthday"
+          type="date"
+          name="birthday"
           value={formValues.birthday}
           onChange={handleChange}
-          className='mt-1 p-2 w-full border-2 border-gray-300 rounded'
+          className="mt-1 p-2 w-full border-2 border-gray-300 rounded"
         />
       </div>
-      <div className='mb-5'>
-        <label className='block text-gray-700'>Kolor:</label>
+      <div className="mb-5">
+        <label className="block text-gray-700">Kolor:</label>
         <input
-          title='color'
-          type='text'
-          name='color'
+          title="color"
+          type="text"
+          name="color"
           value={formValues.color}
           onChange={handleChange}
-          className='mt-1 p-2 w-full border-2 border-gray-300 rounded'
+          className="mt-1 p-2 w-full border-2 border-gray-300 rounded"
         />
       </div>
-      <div className='mb-5'>
-        <label className='block text-gray-700'>Grupa krwi:</label>
+      <div className="mb-5">
+        <label className="block text-gray-700">Grupa krwi:</label>
         <input
-          title='blood'
-          type='text'
-          name='blood'
+          title="blood"
+          type="text"
+          name="blood"
           value={formValues.blood}
           onChange={handleChange}
-          className='mt-1 p-2 w-full border-2 border-gray-300 rounded'
+          className="mt-1 p-2 w-full border-2 border-gray-300 rounded"
         />
       </div>
-      <div className='mb-5'>
-        <label className='block text-gray-700'>Ojciec:</label>
+      <div className="mb-5">
+        <label className="block text-gray-700">Ojciec:</label>
         <input
-          title='father'
-          type='text'
-          name='father'
+          title="father"
+          type="text"
+          name="father"
           value={formValues.father}
           onChange={handleChange}
-          className='mt-1 p-2 w-full border-2 border-gray-300 rounded'
+          className="mt-1 p-2 w-full border-2 border-gray-300 rounded"
         />
       </div>
-      <div className='mb-5'>
-        <label className='block text-gray-700'>Matka:</label>
+      <div className="mb-5">
+        <label className="block text-gray-700">Matka:</label>
         <input
-          title='Mother'
-          type='text'
-          name='Mother'
+          title="Mother"
+          type="text"
+          name="Mother"
           value={formValues.Mother}
           onChange={handleChange}
-          className='mt-1 p-2 w-full border-2 border-gray-300 rounded'
+          className="mt-1 p-2 w-full border-2 border-gray-300 rounded"
         />
       </div>
 
-      <div className='mb-5'>
-        <label className='block text-gray-700'>Opis:</label>
+      <div className="mb-5">
+        <label className="block text-gray-700">Opis:</label>
         <textarea
-          title='description'
-          name='description'
+          title="description"
+          name="description"
           value={formValues.description}
           onChange={handleChange}
-          className='mt-1 p-2 w-full border-2 border-gray-300 rounded'
+          className="mt-1 p-2 w-full border-2 border-gray-300 rounded"
         />
       </div>
       <button
-        type='submit'
-        className='w-full py-2 px-4 bg-blue-600 text-white rounded hover:bg-blue-700'
+        type="submit"
+        className="w-full py-2 px-4 bg-blue-600 text-white rounded hover:bg-blue-700"
       >
         Save
       </button>
