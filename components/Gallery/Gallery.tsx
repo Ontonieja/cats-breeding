c30c91224a8256f72f09290245034badf51c3cef
Maksymilian Rusnak <ontonieja@gmail.com>
2024-04-16 15:32:01 +0200

    gallery styling change

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -5,12 +5,12 @@ const Gallery = async () => {
   const catsImages = await getGalleryImages();
 
   return (
-    <div className="sm:grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 auto-rows-[200px] gap-4 my-10 overflow-x-scroll sm:overflow-visible flex flex-nowrap sm:flex-wrap">
+    <div className="sm:grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 auto-rows-[200px] gap-4 my-10 overflow-x-scroll sm:overflow-visible flex flex-nowrap sm:flex-wrap max-container">
       {catsImages.map(({ url, id }, index) => {
         return (
           <div
             key={id}
-            className={`h-full shadow-card w-full rounded-xl overflow-hidden max-sm:min-w-[80%] ${
+            className={`h-full shadow-card w-full rounded-xl overflow-hidden max-sm:min-w-[90%] ${
               index === 2
                 ? 'sm:row-start-1 sm:row-end-3 sm:col-start-2 sm:col-end-4'
                 : ''
