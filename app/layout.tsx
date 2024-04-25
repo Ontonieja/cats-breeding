eb4401683fb33ffc7d6723e98700471e9f785913
„SmgTR” <smagokpnd@gmail.com>
2024-04-25 15:53:34 +0200

    add favicon to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react';
 import './globals.css';
 import localFont from 'next/font/local';
-import { PawLogo } from '@/public/Icons';
 
 const madimiFont = localFont({
   src: '../public/fonts/MadimiOne-Regular.ttf',
@@ -13,14 +12,14 @@ export const metadata = {
   title: 'Samo Szczęście ',
   description:
     'Odwiedź hodowlę kotów brytyjskich Samo Szczęście i odkryj niezrównaną przyjaźń i urok tych uroczych futrzaków. Z naszą pasją i troską, wprowadź do swojego życia odrobinę kociej magii. Zapraszamy do poznania naszych pięknych kotów brytyjskich, którzy przynoszą radość i miłość w każdy kąt twojego domu.',
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html suppressHydrationWarning className={`${madimiFont.variable}`}>
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body>
         <div>{children}</div>
       </body>
