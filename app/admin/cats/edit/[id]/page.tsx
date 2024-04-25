aa367299f13187f5e38865fc9dd7bccce3b98e8b
Maksymilian Rusnak <ontonieja@gmail.com>
2024-04-25 09:29:27 +0200

    added console.log

--- app/admin/cats/edit/[id]/page.tsx
+++ app/admin/cats/edit/[id]/page.tsx
@@ -198,6 +198,7 @@ const EditCat: FC = () => {
 
   useEffect(() => {
     const fetchImages = async () => {
+      console.log('fetchuje');
       const images = await getCatPhotos(Number(id));
       const galleryImages = await getCatGalleryPhotos(Number(id));
       const lineage = await getCatLinegaeDocument(Number(id));
